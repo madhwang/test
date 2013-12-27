8670ac1918315639144ac9f5245305813f4769a8
madhwang <madhwang@gmail.com>
2013-12-27 11:36:31 +0900

    수정

---
+++ src/java/net/madhwang/timecard/core/dao/MemberDAO.java
@@ -0,0 +1,29 @@
+package net.madhwang.timecard.core.dao;
+
+import javax.annotation.Resource;
+
+import net.madhwang.timecard.core.Member;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class MemberDAO {
+
+	
+	private static final String NAME_SPACE ="net.madhwang.core.dao.Member.";
+	
+	@Resource(name="sqlSessionTemplate")
+	private SqlSession sqlSession;
+	
+	
+	
+	public Member selectOne(final Integer memberNo)
+	{
+		Member member = new Member();
+		member.setMemberNo(memberNo);
+		sqlSession.selectOne(NAME_SPACE+"select",member);
+		return member;
+	}
+}
